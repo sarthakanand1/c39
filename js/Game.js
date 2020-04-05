c0ab8d11699d128540278d2c1ecd59ca47b3df5d
Your Name <you@example.com>
2020-04-05 10:43:06 +0530

    images

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1img)
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2img);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3img);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("blue");
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +83,15 @@ class Game {
       player.distance +=10
       player.update();
     }
+if(player.distance>4000){
+  gameState=2;
 
+}
     drawSprites();
   }
+  end(){
+    console.log("game ended");
+    game.update(2);
+  }
+
 }
